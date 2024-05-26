b67f89ff5c351e5ff83a9225b8abd94f85fb9973
Mahesh-74 <mahesh74.java@gmail.com>
2024-05-26 20:26:02 +0530

    FM

---
+++ src/main/java/com/movie/info/service/controller/MovieResource.java
@@ -0,0 +1,19 @@
+package com.movie.info.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.movie.info.service.model.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+	@GetMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "Test Name");
+	}
+
+}
---
+++ src/test/java/com/movie/info/service/eai_1016_movie_info_service/Eai1016MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.info.service.eai_1016_movie_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai1016MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
